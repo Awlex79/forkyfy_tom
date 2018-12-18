697b818d9e6162a450c0d88c735b36c8ad0148aa
Awlex79 <36120830+Awlex79@users.noreply.github.com>
2018-12-17 22:42:25 -0600

    servings

--- src/js/index.js
+++ src/js/index.js
@@ -121,3 +121,15 @@ const controlRecipe = async () => {
 //window.addEventListener('hashchange', controlRecipe);
 //window.addEventListener('load', controlRecipe);
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe)); 
+
+// Handling recipe buttons clicks
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease * ')) {
+        // Decrease button is clicked
+        state.recipe.updateServings('dec');
+    } else if (e.target.matches('.btn-increase, .btn-increase * ')) {
+         // Increase button is clicked
+        state.recipe.updateServings('inc');
+    }
+    console.log(state.recipe);
+})
